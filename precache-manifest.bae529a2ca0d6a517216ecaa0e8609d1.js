80128164ac412e3e1ed1ba6d4d1be7855533d097
asraaijaz-jpg <63543965+asraaijaz-jpg@users.noreply.github.com>
2023-03-20 21:48:56 +0500

    Updates

---
+++ precache-manifest.bae529a2ca0d6a517216ecaa0e8609d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f636b693bf5e342cea30",
+    "url": "/Order-form-/static/css/main.f73eeefc.chunk.css"
+  },
+  {
+    "revision": "f636b693bf5e342cea30",
+    "url": "/Order-form-/static/js/main.f636b693.chunk.js"
+  },
+  {
+    "revision": "7898acd3e2a41c3882ae",
+    "url": "/Order-form-/static/css/1.9b09646a.chunk.css"
+  },
+  {
+    "revision": "7898acd3e2a41c3882ae",
+    "url": "/Order-form-/static/js/1.7898acd3.chunk.js"
+  },
+  {
+    "revision": "976b7d8a9221c52be311",
+    "url": "/Order-form-/static/js/runtime~main.976b7d8a.js"
+  },
+  {
+    "revision": "37d442e394fddc70b982de6bd2fa4b29",
+    "url": "/Order-form-/index.html"
+  }
+];
